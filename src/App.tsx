0e0fe993c163d73a68511e7f826056a97470988d
proggy-al <proggy-al@yandex.ru>
2024-04-15 17:16:40 +0300

    All ready

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import {AvtoForm} from './components/AvtoForm';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <AvtoForm/>     
+    </div>
+  );
+}
+
+export default App;
